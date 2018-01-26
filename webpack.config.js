0e59fd94814fb6fc4f465f644d050c5d4aa6241a
Andrei Cimpoesu <andrei.cimpoesuu@gmail.com>
2018-01-26 15:58:51 +0200

    webpack: more config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-    entry: './src/index.tsx',
-    entry: {
-        client: './src/client/app.tsx',
-        server: './src/server/app.tsx',
-    },
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist')
-    }, 
-    devtool: 'source-map',       
-	module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader", exclude: /node_modules/ },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
-    },
+
+const serverConfig = {
     target: 'node',
-    externals: [nodeExternals()]
+    entry: './src/server/app.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'server.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
+    },    
+    externals: [nodeExternals()],
+    watch: true,
+    stats: 'errors-only',    
 };
+
+const clientConfig = {
+    target: 'web',
+    entry: './src/client/app.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'client.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
+    },
+    watch: true,
+    stats: 'errors-only',        
+};
+
+module.exports = [serverConfig, clientConfig];
